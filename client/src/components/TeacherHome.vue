2fc0a9d8e28b84247b1f00ce0a47179b07be492f
Sajidul Kabir <sajidulkabiradib@gmail.com>
2022-08-31 16:48:49 +0600

    project initialized wireframe chosen

---
+++ client/src/components/TeacherHome.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app left>
+      <template v-slot:prepend>
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/women/81.jpg" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Dr. Ahsan Habib</v-list-item-title>
+            <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="#43a047"
+      dark
+      shrink-on-scroll
+      src="../assets/sust-photo.png"
+      fade-img-on-scroll
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Welcome to Vehicle Requisition Service</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TeacherHome",
+
+  data: () => ({
+    drawer: false,
+    items: [
+      { title: "Home", icon: "mdi-home-city" },
+      { title: "My Account", icon: "mdi-account" },
+      { title: "Users", icon: "mdi-account-group-outline" },
+    ],
+  }),
+};
+</script>
+
+<style scoped></style>
