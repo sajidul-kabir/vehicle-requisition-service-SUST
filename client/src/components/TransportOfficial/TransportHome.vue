55e8c22f28e62c78e4a6ad658740517056cb80da
Sajidul Kabir <sajidulkabiradib@gmail.com>
2022-09-23 18:25:19 +0600

    tabluar view added for requisitions list

--- client/src/components/TransportOfficial/TransportHome.vue
+++ client/src/components/TransportOfficial/TransportHome.vue
@@ -5,68 +5,105 @@
         <v-toolbar-title>See Requisition List</v-toolbar-title>
       </template>
     </transport-nav>
+    <div class="ml-12 mt-12">
+      <v-btn outlined color="indigo" :class="viewCheck" @click="gridBtn"
+        >Grid view</v-btn
+      ><v-btn
+        outlined
+        color="indigo"
+        :class="viewCheck2"
+        @click="tabBtn"
+        style="margin-left: -2px"
+        >Tabular view</v-btn
+      >
+    </div>
 
-    <v-card class="ml-12 mt-12" max-width="1000" elevation="2" outlined>
-      <v-card-text class="ml-2">
-        <div class="text-flex">
-          <div>3s ago</div>
-          <div>Requisition ID #335</div>
-        </div>
+    <div v-if="view === 'grid'">
+      <v-card class="ml-12 mt-12" max-width="1000" elevation="2" outlined>
+        <v-card-text class="ml-2">
+          <div class="text-flex">
+            <div>3s ago</div>
+            <div>Requisition ID #335</div>
+          </div>
 
-        <p class="text-h5 text--primary">
-          Dr. Ahsan Habib has Submitted a Requisition Form
-        </p>
-        <p class="text--primary mb-0">Date: 3/09/2022 ~ 8.00 AM - 11.00 AM</p>
-        <p class="text--primary mb-0">Destination: Ambarkhana</p>
-        <p class="text--primary mb-0">Official Need</p>
-      </v-card-text>
+          <p class="text-h5 text--primary">
+            Dr. Ahsan Habib has Submitted a Requisition Form
+          </p>
+          <p class="text--primary mb-0">Date: 3/09/2022 ~ 8.00 AM - 11.00 AM</p>
+          <p class="text--primary mb-0">Destination: Ambarkhana</p>
+          <p class="text--primary mb-0">Official Need</p>
+        </v-card-text>
 
-      <v-card-actions class="mb-2 ml-2">
-        <router-link to="/transport-home/335">
+        <v-card-actions class="mb-2 ml-2">
+          <router-link to="/transport-home/335">
+            <v-btn outlined color="indigo" class="details">See Details</v-btn>
+          </router-link>
+        </v-card-actions>
+      </v-card>
+
+      <v-card class="ml-12 mt-12" max-width="1000" elevation="2" outlined>
+        <v-card-text class="ml-2">
+          <div class="text-flex">
+            <div>5min ago</div>
+            <div>Requisition ID #336</div>
+          </div>
+
+          <p class="text-h5 text--primary">
+            Dr. Md Forhad Rabbi has Submitted a Requisition Form
+          </p>
+          <p class="text--primary mb-0">Date: 3/09/2022 ~ 10.00 AM - 1.00 PM</p>
+          <p class="text--primary mb-0">Destination: Airport</p>
+          <p class="text--primary mb-0">Official Need</p>
+        </v-card-text>
+
+        <v-card-actions class="mb-2 ml-2">
           <v-btn outlined color="indigo" class="details">See Details</v-btn>
-        </router-link>
-      </v-card-actions>
-    </v-card>
+        </v-card-actions>
+      </v-card>
 
-    <v-card class="ml-12 mt-12" max-width="1000" elevation="2" outlined>
-      <v-card-text class="ml-2">
-        <div class="text-flex">
-          <div>5min ago</div>
-          <div>Requisition ID #336</div>
-        </div>
+      <v-card class="ml-12 mt-12" max-width="1000" elevation="2" outlined>
+        <v-card-text class="ml-2">
+          <div class="text-flex">
+            <div>2hrs ago</div>
+            <div>Requisition ID #337</div>
+          </div>
 
-        <p class="text-h5 text--primary">
-          Dr. Md Forhad Rabbi has Submitted a Requisition Form
-        </p>
-        <p class="text--primary mb-0">Date: 3/09/2022 ~ 10.00 AM - 1.00 PM</p>
-        <p class="text--primary mb-0">Destination: Airport</p>
-        <p class="text--primary mb-0">Official Need</p>
-      </v-card-text>
+          <p class="text-h5 text--primary">
+            Enamul Hassan has Submitted a Requisition Form
+          </p>
+          <p class="text--primary mb-0">Date: 4/09/2022 ~ 8.00 AM - 11.00 AM</p>
+          <p class="text--primary mb-0">Destination: Ambarkhana</p>
+          <p class="text--primary mb-0">Personal Need</p>
+        </v-card-text>
 
-      <v-card-actions class="mb-2 ml-2">
-        <v-btn outlined color="indigo" class="details">See Details</v-btn>
-      </v-card-actions>
-    </v-card>
+        <v-card-actions class="mb-2 ml-2">
+          <v-btn outlined color="indigo" class="details">See Details</v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
 
-    <v-card class="ml-12 mt-12" max-width="1000" elevation="2" outlined>
-      <v-card-text class="ml-2">
-        <div class="text-flex">
-          <div>2hrs ago</div>
-          <div>Requisition ID #337</div>
-        </div>
-
-        <p class="text-h5 text--primary">
-          Enamul Hassan has Submitted a Requisition Form
-        </p>
-        <p class="text--primary mb-0">Date: 4/09/2022 ~ 8.00 AM - 11.00 AM</p>
-        <p class="text--primary mb-0">Destination: Ambarkhana</p>
-        <p class="text--primary mb-0">Personal Need</p>
-      </v-card-text>
-
-      <v-card-actions class="mb-2 ml-2">
-        <v-btn outlined color="indigo" class="details">See Details</v-btn>
-      </v-card-actions>
-    </v-card>
+    <div v-else>
+      <v-card class="ml-12 mt-12">
+        <v-card-title>
+          Requisition Overview
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="requisitions"
+          :search="search"
+          @click:row="rowClick"
+          class="d-tab"
+        ></v-data-table>
+      </v-card>
+    </div>
   </div>
 </template>
 
@@ -74,8 +111,90 @@
 import TransportNav from "./TransportNav.vue";
 export default {
   data: () => ({
+    view: "grid",
     show: false,
+
+    search: "",
+    headers: [
+      {
+        text: "Requisition ID",
+        align: "start",
+        sortable: true,
+        value: "name",
+      },
+      { text: "Teachers name", value: "teacherName" },
+      { text: "Start-time", value: "startTime" },
+      { text: "End-time", value: "endTime" },
+      { text: "Date", value: "date" },
+      { text: "Destination", value: "dest" },
+      { text: "Need", value: "need" },
+    ],
+    requisitions: [
+      {
+        name: "ID #335",
+        teacherName: "Dr. Ahsan Habib",
+        startTime: "8.00",
+        endTime: "11.00",
+        date: "24/09/2022",
+        dest: "Ambarkhana",
+        need: "Official",
+      },
+      {
+        name: "ID #336",
+        teacherName: "Dr. X",
+        startTime: "8.00",
+        endTime: "11.00",
+        date: "24/09/2022",
+        dest: "Shubidbazar",
+        need: "Personal",
+      },
+      {
+        name: "ID #337",
+        teacherName: "Dr. Y",
+        startTime: "13.00",
+        endTime: "15.00",
+        date: "24/09/2022",
+        dest: "Ambarkhana",
+        need: "Official",
+      },
+      {
+        name: "ID #338",
+        teacherName: "Dr. Ahsan Habib",
+        startTime: "10.00",
+        endTime: "13.00",
+        date: "25/09/2022",
+        dest: "Airport",
+        need: "Personal",
+      },
+    ],
   }),
+
+  methods: {
+    gridBtn() {
+      this.view = "grid";
+    },
+    tabBtn() {
+      this.view = "tab";
+    },
+    rowClick(d1, d2) {
+      console.log(d1, d2);
+    },
+  },
+  computed: {
+    viewCheck() {
+      if (this.view === "grid") {
+        return "tab-btn";
+      }
+      return "";
+    },
+    viewCheck2() {
+      if (this.view === "tab") {
+        return "tab-btn";
+      }
+      return "";
+    },
+  },
+
   components: {
     TransportNav,
   },
@@ -90,5 +209,15 @@ a {
   display: flex;
   justify-content: space-between;
   margin-bottom: 15px;
+}
+.tab-btn {
+  background-color: teal;
+  color: white !important;
+  height: 37px !important;
+}
+.d-tab {
+}
+.d-tab:hover {
+  cursor: pointer;
 }
 </style>
